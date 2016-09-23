d8fd67f181845b772f7d6b60292d61bba4f95e53
vitasar <bubyakin.v@gmail.com>
2016-09-23 15:43:43 +0300

    Gulpfile v.3.2.

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,39 @@
+/* jshint node: true */
 "use strict";
 
 var gulp = require("gulp"),
   sass = require("gulp-sass"),
   postcss = require("gulp-postcss"),
-  // it adds browser prefixes
+  // add browser prefixes
   autoprefixer = require("autoprefixer"),
-  // it reorders css options in wished order
+  // reorder css rules in wished order
   csscomb = require("gulp-csscomb"),
   // css minifier
   nano = require("gulp-cssnano"),
-  // it helps rename files/
+  // help rename files/
   // for example, adding suffix .min to files
   rename = require("gulp-rename"),
   // image minifier
   imagemin = require("gulp-imagemin"),
   // js minifier
   uglify = require("gulp-uglify"),
-  // it replaces one string in the file to another.
+  // replace one string in the file to another.
   replace = require("gulp-replace"),
-  // it hepls to execute tasks in the right order
+  // execute tasks in the right order
   runSequence = require("run-sequence"),
   // simple template engine
   pagebuilder = require("gulp-pagebuilder"),
   // livereload
   browserSync = require("browser-sync"),
   reload = browserSync.reload,
-  // it organize media querries at the end of the style file
+  // transfer media queries to the end style file
   combineMq = require("gulp-combine-mq"),
-  // it removes files
+  // remove files
   del = require("del"),
-  // it turns relative path to file into filename
+  // turn relative path scripts to absolute (for bower puproses)
   flatten = require("gulp-flatten"),
   // custom realization standart watch.
-  // gulp doesn't notice files created after launching gulp.
+  // gulp doesn't notice files which created after launching gulp.
   // this plugin resolves this issue.
   watch = require("gulp-watch"),
   // streaming helper for gulp-watch.
@@ -75,19 +76,21 @@ var path = {
 
 gulp.task("build:style", function () {
   return gulp.src([
-        path.src.style.self + "case.scss",
-        path.src.style.self + "blog.scss"
+      path.src.style.self + "case.scss",
+      path.src.style.self + "blog.scss"
     ])
-    .pipe(sass({
-        includePaths: normalize.includePaths
-    }).on("error", sass.logError))
-    .pipe(postcss([autoprefixer({browsers: ["last 4 versions"]})]))
-    .pipe(csscomb())
-    .pipe(combineMq())
-    .pipe(gulp.dest(path.build.css))
-    .pipe(nano())
-    .pipe(rename({ suffix: ".min"}))
-    .pipe(gulp.dest(path.build.css))
+    .pipe( sass({
+      includePaths: normalize.includePaths
+    }).on("error", sass.logError) )
+    .pipe( postcss( [
+      autoprefixer({browsers: ["last 4 versions"]})
+    ]) )
+    .pipe( csscomb() )
+    .pipe( combineMq() )
+    .pipe( gulp.dest(path.build.css) )
+    .pipe( nano({discardComments: {removeAll: true}}) )
+    .pipe( rename({ suffix: ".min"}) )
+    .pipe( gulp.dest(path.build.css) )
 });
 
 gulp.task("style", function () {
@@ -95,64 +98,66 @@ gulp.task("style", function () {
     // if in the project there are several basic style files
     // the variable 'path.src.style.self' is equal to folder,
     // which contains the files. in other case the one is equal to path to the basic style file.
-        path.src.style.self + "case.scss",
-        path.src.style.self + "blog.scss"
+      path.src.style.self + "case.scss",
+      path.src.style.self + "blog.scss"
     ])
-    .pipe(sass({
-        outputStyle: "expanded",
-        includePaths: normalize.includePaths
-    }).on("error", sass.logError))
-    .pipe(postcss([autoprefixer({browsers: ["last 4 versions"]})]))
-    .pipe(csscomb())
-    .pipe(combineMq())
-    .pipe(gulp.dest(path.build.css))
-    .pipe(reload({
+    .pipe( sass({
+      outputStyle: "expanded",
+      includePaths: normalize.includePaths
+    }).on("error", sass.logError) )
+    .pipe( postcss([
+      autoprefixer({browsers: ["last 4 versions"]})
+    ]) )
+    .pipe( csscomb() )
+    .pipe( combineMq() )
+    .pipe( gulp.dest(path.build.css) )
+    .pipe( reload({
       stream: true
-    }))
+    }) )
 });
 
 gulp.task("html", function () {
   return gulp.src(path.src.html.self)
-    .pipe(pagebuilder(path.src.self))
-    .pipe(gulp.dest(path.build.html))
-    .pipe(reload({
+    .pipe( pagebuilder(path.src.self) )
+    .pipe( gulp.dest(path.build.html) )
+    .pipe( reload({
       stream: true
-    }))
+    }) )
 });
 
 gulp.task("build:html", function () {
   return gulp.src(path.src.html.self)
-    .pipe(pagebuilder(path.src.self))
-    .pipe(replace(/.css\"/g, ".min.css\""))
-    .pipe(replace(/.js\"/g, ".min.js\""))
-    .pipe(gulp.dest(path.build.html))
+    .pipe( pagebuilder(path.src.self) )
+    .pipe( replace(/.css\"/g, ".min.css\"") )
+    .pipe( replace(/.js\"/g, ".min.js\"") )
+    .pipe( gulp.dest(path.build.html) )
 });
 
 gulp.task("fonts", function () {
   return gulp.src(path.src.fonts)
-    .pipe(gulp.dest(path.build.fonts))
-    .pipe(reload({
+    .pipe( gulp.dest(path.build.fonts) )
+    .pipe( reload({
       stream: true
-    }))
+    }) )
 });
 
 gulp.task("img", function () {
   return gulp.src(path.src.img)
-    .pipe(gulp.dest(path.build.img))
-    .pipe(reload({
+    .pipe( gulp.dest(path.build.img) )
+    .pipe( reload({
       stream: true
-    }))
+    }) )
 });
 
 gulp.task("build:img", function () {
   return gulp.src(path.src.img)
-    .pipe(imagemin({
+    .pipe( imagemin({
       progressive: true,
       optimizationLevel: 3,
       svgoPlugins: [{cleanupIDs: false}],
       multipass: true
-    }))
-    .pipe(gulp.dest(path.build.img))
+    }) )
+    .pipe( gulp.dest(path.build.img) )
 });
 
 gulp.task("build:js", function () {
@@ -162,12 +167,12 @@ gulp.task("build:js", function () {
       path.src.script.bower + "mustache.min.js",*/
       path.src.script.self
     ])
-    .pipe(pagebuilder(path.src.self))
-    .pipe(flatten())
-    .pipe(gulp.dest(path.build.js))
-    .pipe(rename({ suffix: ".min"}))
-    .pipe(uglify())
-    .pipe(gulp.dest(path.build.js))
+    .pipe( pagebuilder(path.src.self) )
+    .pipe( flatten() )
+    .pipe( gulp.dest(path.build.js) )
+    .pipe( rename({ suffix: ".min"}) )
+    .pipe( uglify() )
+    .pipe( gulp.dest(path.build.js) )
 });
 
 gulp.task("js", function () {
@@ -177,12 +182,12 @@ gulp.task("js", function () {
       path.src.script.bower + "mustache.min.js",*/
       path.src.script.self
     ])
-    .pipe(pagebuilder(path.src.self))
-    .pipe(flatten())
-    .pipe(gulp.dest(path.build.js))
-    .pipe(reload({
+    .pipe( pagebuilder(path.src.self) )
+    .pipe( flatten() )
+    .pipe( gulp.dest(path.build.js) )
+    .pipe( reload({
       stream: true
-    }))
+    }) )
 });
 
 gulp.task("clean", function () {
@@ -207,34 +212,34 @@ gulp.task("server", function () {
 
 gulp.task("watch", function() {
   watch(path.src.style.folder, batch(function(events, done) {
-      gulp.start("style", done);
+    gulp.start("style", done);
   }));
   watch(path.src.html.folder, batch(function(events, done) {
-      gulp.start("html", done);
+    gulp.start("html", done);
   }));
   watch(path.src.fonts, batch(function(events, done) {
-      gulp.start("fonts", done);
+    gulp.start("fonts", done);
   }));
   watch(path.src.img, batch(function(events, done) {
-      gulp.start("img", done);
+    gulp.start("img", done);
   }));
   watch(path.src.script.folder, batch(function(events, done) {
-      gulp.start("js", done);
+    gulp.start("js", done);
   }));
 })
 
 gulp.task("build", function () {
-    runSequence(
-        "clean",
-        [
-            "build:style",
-            "build:js",
-            "build:html",
-            "build:img",
-            // fonts:build task doesn't diffs from fonts, therefore it doesn't exist.
-            "fonts"
-        ]
-    );
+  runSequence(
+    "clean",
+    [
+      "build:style",
+      "build:js",
+      "build:html",
+      "build:img",
+      // fonts:build task doesn't diffs from fonts, therefore it doesn't exist.
+      "fonts"
+    ]
+  );
 });
 
 gulp.task("default", function() {
